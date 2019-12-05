2ffa56988744dc7ae8adacc39fcaaaf821c204bb
Nico Virgino <nicovirgino@gmail.com>
2019-12-05 15:54:17 -0600

    added user interaction

--- src/grocery/GroceryListApplication.java
+++ src/grocery/GroceryListApplication.java
@@ -4,6 +4,7 @@ import util.Input;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class GroceryListApplication {
 
@@ -22,36 +23,50 @@ public class GroceryListApplication {
 
         Input input = new Input();
         boolean exited = false;
+//TODO: if they pick yes, they will be prompted if they would like to enter a new item
+        //prompt user if they want a new item
+        System.out.println("Would you like to make a new list? [y/n]");
+        if (input.yesNo()) {
+            //prompt user for category
+            //TODO: if the user's pick yes, they should be given the following three prompts:
+            System.out.println("Select a category: ");
+            System.out.println(input.getString());
+            System.out.println("Enter name of item: ");
+            System.out.println(input.getString());
+            System.out.println("Enter how many you want: ");
+            System.out.println(input.getInt());
 
-        if (Input.yesNo("Would you like to create a grocery list y/n?")) {
 
+            //declare hash map for groceries
             HashMap<String, ArrayList<String>> groceries = new HashMap<>();
-
             groceries.put("Bakery", new ArrayList<>());
             groceries.put("Fruit", new ArrayList<>());
             groceries.put("Meat", new ArrayList<>());
             groceries.put("Vegetables", new ArrayList<>());
-            do {
-                if (input.yesNo("Would you like to enter a new item y/n?")) {
-                    String category;
-                    String name;
-                    int quantity;
+            System.out.println("Would you like to enter a new item [y/n]");
+            input.yesNo();
+            if (input.yesNo()) {
+
+                String category;
+                String name;
+                int quantity;
 
 
-            //TODO: if they pick yes, they will be prompted if they would like to enter a new item
-
-            //TODO: if the user's pick yes, they should be given the following three prompts:
 
 
-            //TODO: given an ordered list of grocery categories to choose from, select the category
-            //TODO: Enter name of the item
-            //TODO: Enter how many of the item
 
 
-            //TODO:  give the user a choice to finalize the list or add an additional item
+
+                //TODO: given an ordered list of grocery categories to choose from, select the category
+                //TODO: Enter name of the item
+                //TODO: Enter how many of the item
 
 
-            //TODO: when finalized, present a complete list organized ALPHABETICALLY and grouped by CATEGORY including QUANTITY
+                //TODO:  give the user a choice to finalize the list or add an additional item
+
+
+                //TODO: when finalized, present a complete list organized ALPHABETICALLY and grouped by CATEGORY including QUANTITY
+            }
         }
     }
 }
